44590f2826c24202b9f7350a741856d72a9b8b57
Kanade <andy_wang0625@hotmail.com>
2020-08-12 17:16:34 -0400

    Update Message.cpp

--- Core/Message.cpp
+++ Core/Message.cpp
@@ -1,9 +1,22 @@
 /*
  * @Author: Kanade
  * @Date: 2020-08-10 17:24:01
- * @LastEditTime: 2020-08-10 17:34:46
+ * @LastEditTime: 2020-08-12 17:16:19
  * @Description: 
  */
 #include"Message.h"
+#include"../Miscellaneous/Log.h"
+#include<string>
 using namespace std;
 
+Log* logHelper;
+
+Message::Message(string message, string sender, DateTime time){
+    _messageBody = message;
+    _messageSender = message;
+    _time = time;
+}
+
+string Message::toString(){
+    string str = _time.toString()+": ("+_messageSender+") "+_messageBody;
+}
--- Core/Message.h
+++ Core/Message.h
@@ -1,12 +1,28 @@
 /*
  * @Author: Kanade
  * @Date: 2020-08-10 17:23:55
- * @LastEditTime: 2020-08-10 17:25:17
+ * @LastEditTime: 2020-08-12 17:12:42
  * @Description: 
  */
 #include<string>
-using namespace std;
+#include"../Miscellaneous/DateTime.h"
+#include"../Miscellaneous/Log.h"
+
+Log* logHelper;
+
 #ifndef MESSAGE_H
 #define MESSAGE_H
+
+class Message{
+    public:
+        Message(string, string, DateTime);
+        ~Message();
+        string toString();
+    private:
+        string _messageBody;
+        string _messageSender;
+        DateTime _time;
+}
+
 
 #endif
